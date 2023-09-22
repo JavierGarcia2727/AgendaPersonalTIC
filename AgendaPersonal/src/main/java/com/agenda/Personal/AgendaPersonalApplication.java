0fe6ebcd0bcc9c7737fa3a98bd8c8d03f7353840
Javi <marvin_garcia27@hotmail.com>
2023-09-21 23:01:38 -0600

    ya tiene la paginacion la tabla contactos

--- AgendaPersonal/src/main/java/com/agenda/Personal/AgendaPersonalApplication.java
+++ AgendaPersonal/src/main/java/com/agenda/Personal/AgendaPersonalApplication.java
@@ -2,12 +2,19 @@ package com.agenda.Personal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class AgendaPersonalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaPersonalApplication.class, args);
+		
+		
 	}
-
+	   @Bean
+	    public SpringDataDialect springDataDialect() {
+	        return new SpringDataDialect();
+	    }
 }
--- AgendaPersonal/src/main/java/com/agenda/Personal/controller/ContactoController.java
+++ AgendaPersonal/src/main/java/com/agenda/Personal/controller/ContactoController.java
@@ -4,8 +4,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,19 +22,35 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.agenda.Personal.model.Contacto;
 import com.agenda.Personal.repository.ContactoRepository;
 
+import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
+
 @Controller
 @RequestMapping("/contacto")
 public class ContactoController {
 
 	@Autowired
 	private ContactoRepository contactoRepository;
+	/*
+	 * //este metodo es funcional antes de poner la paginacion
+	 * 
+	 * @GetMapping("/index") public String index1(Model model) {
+	 * 
+	 * List<Contacto> contactos = contactoRepository.findAll();
+	 * 
+	 * model.addAttribute("contactos", contactos);
+	 * 
+	 * return "index"; }
+	 */
 
 	@GetMapping("/index")
-	public String index(Model model) {
+	public String index(
+			@PageableDefault(size = 3, sort = "fechaRegistro",
+			direction = Direction.DESC) Pageable pageable,
+			ModelMap model) {
 
-		List<Contacto> contactos = contactoRepository.findAll();
+		Page<Contacto> contactospage = contactoRepository.findAll(pageable);
 
-		model.addAttribute("contactos", contactos);
+		model.addAttribute("contactospage", contactospage);
 
 		return "index";
 	}
@@ -66,46 +87,45 @@ public class ContactoController {
 
 	@GetMapping("/editar/{id}")
 	public String editar(@PathVariable Integer id, Model model) {
-		System.out.println("################ editar su id es ####### "+id);
+		System.out.println("################ editar su id es ####### " + id);
 
 		Contacto contacto = contactoRepository.findById(id).orElse(null);
-		System.out.println("xxxxxxxxxxxxxxx "+contacto.getId());
+		System.out.println("xxxxxxxxxxxxxxx " + contacto.getId());
 		model.addAttribute("contacto", contacto);
 
 		return "Actualizar";
 
 	}
 
-	/*@PostMapping("/actualizar/{id}")
-	public String actualizar(@Validated Contacto contacto, BindingResult bindingResult, RedirectAttributes mensaje,
-			Model model, @PathVariable Integer id) {
-		System.out.println("################ actualizar ####### "+id);
-		Contacto contactoFormDB = contactoRepository.findById(id).orElse(null);
-		
-		contactoFormDB.setNombre(contacto.getNombre());
-		contactoFormDB.setCelular(contacto.getCelular());
-		contactoFormDB.setCorreo(contacto.getCorreo());
-		contactoFormDB.setFechaNacimiento(contacto.getFechaNacimiento());
-		
-		
-		if (bindingResult.hasErrors()) {
-			model.addAttribute("contacto", contacto);
-
-			return "editar";
-
-		}
-		contactoRepository.save(contactoFormDB);
-
-		mensaje.addFlashAttribute("msgExito", "El contacto se ha actualizado correctamente");
-
-		return "redirect:/contacto/index";
-	}*/
+	/*
+	 * @PostMapping("/actualizar/{id}") public String actualizar(@Validated Contacto
+	 * contacto, BindingResult bindingResult, RedirectAttributes mensaje, Model
+	 * model, @PathVariable Integer id) {
+	 * System.out.println("################ actualizar ####### "+id); Contacto
+	 * contactoFormDB = contactoRepository.findById(id).orElse(null);
+	 * 
+	 * contactoFormDB.setNombre(contacto.getNombre());
+	 * contactoFormDB.setCelular(contacto.getCelular());
+	 * contactoFormDB.setCorreo(contacto.getCorreo());
+	 * contactoFormDB.setFechaNacimiento(contacto.getFechaNacimiento());
+	 * 
+	 * 
+	 * if (bindingResult.hasErrors()) { model.addAttribute("contacto", contacto);
+	 * 
+	 * return "editar";
+	 * 
+	 * } contactoRepository.save(contactoFormDB);
+	 * 
+	 * mensaje.addFlashAttribute("msgExito",
+	 * "El contacto se ha actualizado correctamente");
+	 * 
+	 * return "redirect:/contacto/index"; }
+	 */
 	@PostMapping("/actualizar")
 	public String actualizar(@Validated Contacto contacto, BindingResult bindingResult, RedirectAttributes mensaje,
 			Model model) {
-		System.out.println("================== actualizar ####### "+contacto.getId());
-		
-		
+		System.out.println("================== actualizar ####### " + contacto.getId());
+
 		if (bindingResult.hasErrors()) {
 			model.addAttribute("contacto", contacto);
 
@@ -118,11 +138,11 @@ public class ContactoController {
 
 		return "redirect:/contacto/index";
 	}
-	
-	@PostMapping("/eleminar/{id}")
+
+	@PostMapping("/eliminar/{id}")
 	public String eliminar(@PathVariable Integer id, RedirectAttributes mensaje) {
-	
-				
+		System.out.println("================== Eliminart ####### " + id);
+
 		Contacto contacto = contactoRepository.findById(id).orElse(null);
 		contactoRepository.delete(contacto);
 
@@ -130,6 +150,5 @@ public class ContactoController {
 
 		return "redirect:/contacto/index";
 	}
-	
 
 }
